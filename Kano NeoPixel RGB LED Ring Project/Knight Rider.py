eb76cfb6a598ebf36cd6a15c965d5793fdc95063
Jai.z Tech Domain <113267953+JaizTechDomain@users.noreply.github.com>
2024-02-27 16:28:07 +1100

    updated code

--- Kano NeoPixel RGB LED Ring Project/Knight Rider.py
+++ Kano NeoPixel RGB LED Ring Project/Knight Rider.py
@@ -1,20 +1,23 @@
-# adpated from Nerd Cave ( Guitarman9119 on github )
-
-
 from neopixel import Neopixel
 import utime
 
 numpix = 10
 strip = Neopixel(numpix, 0, 28, "RGB")
 
-red = (255, 0, 0)
+delay = 0.040
 
-delay = 0.004
+utime.sleep(delay)
 
-utime.sleep(delay)  
+red = (0, 255, 0)
+orange = (255, 50, 0)
+yellow = (255, 100, 0)
+green = (255, 0, 0)
+blue = (0, 0, 255)
+indigo = (100, 0, 90)
+violet = (0, 175, 255)
+colors_rgb = [red, orange, yellow, green, blue, indigo, violet]
 
-
-strip.brightness(42)
+strip.brightness(47)
 blank = (0,0,0)
 
  
@@ -23,43 +26,45 @@ while True:
  
     strip.show()
   ############ Left to Right ##################  
-    for x in range(18):
-        strip.set_pixel(x+1, red)
+    for x in range(10):
+        strip.set_pixel(x, violet)
         strip.show()
         utime.sleep(delay)
-        strip.set_pixel(x, red)
+        strip.set_pixel(x, violet)
         strip.show()
         utime.sleep(delay)
-        strip.set_pixel(x+2, red)
+        strip.set_pixel(x, violet)
         strip.show()
         utime.sleep(delay)
         strip.set_pixel(x, blank)
         utime.sleep(delay)
-        strip.set_pixel(x+1, blank)
+        strip.set_pixel(x, blank)
         utime.sleep(delay)
-        strip.set_pixel(x+2, blank)
+        strip.set_pixel(x, blank)
         strip.show()
+        
+          
         
 
         
   ############ Left to Right ##################  
-    for x in reversed(range(18)):       
-        strip.set_pixel(x+1, red)
+    for x in reversed(range(9)):       
+        strip.set_pixel(x, violet)
         utime.sleep(delay)
         strip.show()
-        strip.set_pixel(x, red)
+        strip.set_pixel(x, violet)
         utime.sleep(delay)
         strip.show()
-        strip.set_pixel(x+2, red)
+        strip.set_pixel(x, violet)
         utime.sleep(delay)
         strip.show()
+        utime.sleep(delay)
         strip.set_pixel(x, blank)
         utime.sleep(delay)
-        strip.set_pixel(x+1, blank)
-        utime.sleep(delay) 
-        strip.set_pixel(x+2, blank)
+        strip.set_pixel(x, blank)
+        utime.sleep(delay)
+        strip.set_pixel(x, blank)
         strip.show()
         
           
-
 
